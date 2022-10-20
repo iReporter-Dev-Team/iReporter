d42afcdfe8ae0ff7429ad3878bbb7db93ac97cf0
GLADYS <mungaiglado106@gmail.com>
2022-10-20 18:21:37 +0300

    added reflags

---
+++ app/controllers/redflags_controller.rb
@@ -0,0 +1,35 @@
+class RedflagsController < ApplicationController
+    def index
+        render json: Redflag.all, status: :ok
+    end
+
+    def create
+        redflag = Redflag.create(redflag_params)
+        if redflag.valid?
+            render json: redflag, status: :created
+        else
+            render json: { errors: redflag.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        redflag = Redflag.find(params[:id])
+        if redflag.update(redflag_params)
+            render json: redflag, status: :ok
+        else
+            render json: { errors: redflag.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        redflag = Redflag.find(params[:id])
+        redflag.destroy
+        head :no_content
+    end
+
+    private
+    def
+    redflag_params
+    params.permit(:location, :image, :video, :status, :description, :user_id)
+end
+end
