4905611263d0c52af047f5b90bc28a5791347c7c
GLADYS <mungaiglado106@gmail.com>
2022-10-20 18:18:34 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    private
+    def render_unprocessable_entity_response(exception)
+      render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+
+
 end
