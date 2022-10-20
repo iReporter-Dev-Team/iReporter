80bc45dd0d1e4c3bdc67261342aa3fbcf5f75a1e
GLADYS <mungaiglado106@gmail.com>
2022-10-20 18:22:51 +0300

    added sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+    before_action :authorize
+skip_before_action :authorize, only: :create
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+    private
+    def authorize
+        return render json: { errors: ["Not Logged In"] }, status: :unauthorized unless session.include? :user_id
+
+    end
+end
