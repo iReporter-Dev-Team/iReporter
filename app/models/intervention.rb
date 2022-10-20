0b36ad002e1d5d221e71c71955041e88e6a94a7b
GLADYS <mungaiglado106@gmail.com>
2022-10-20 18:23:35 +0300

    created interventions

---
+++ app/models/intervention.rb
@@ -0,0 +1,9 @@
+class Intervention < ApplicationRecord
+    belongs_to :user
+
+    validates :description, presence: true, length:  {:in => 100..500 }
+    validates :location, presence: true
+    validates :status, presence: true
+    validates :video, presence: true
+    validates :image, presence: true
+end
