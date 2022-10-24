6782534671c2a98004d5ccf9ec19ac66a26320a0
Said Hussein <60461223+Said-cmd@users.noreply.github.com>
2022-10-24 22:58:27 +0300

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import GetStarted from "./GetStarted";
 import DashBoardViewDetails from "./DashBoardViewDetails";
+import SignIn from "./SignIn/SignIn";
+import SignUp from "./SignUp/SignUp";
+import AdminDashboard from "./AdminDashboard";
 
 function App() {
+    const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me") 
+    .then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+  
   return (
-    <Router>
       <div className="row mt-3">
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
@@ -16,9 +29,11 @@ function App() {
             path="/interventions/:interventionId"
             element={<DashBoardViewDetails />}
           ></Route>
+          <Route exact path="/dashboard" element={<AdminDashboard user={user}/>}/>
+          <Route exact path='/login' element={<SignIn onLogin={setUser}/>}/>
+          <Route exact path="/signup" element={<SignUp onLogin={setUser}/>}/>
         </Routes>
       </div>
-    </Router>
   );
 }
 
