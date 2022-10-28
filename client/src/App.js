eb695d02e4bb6f53fb1620110b3deba73dc8eed0
Said Hussein <saidhussein12342@gmail.com>
2022-10-29 00:01:45 +0300

    File cleanup.

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Profile from "./components/Profile/profile";
 import UserLanding from "./User-Landing/UserLanding";
 import UsersList from "./components/UsersList";
 import NotFound from "./components/NotFound";
-import NotAuthorized from "./components/NotAuthorized";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -26,13 +25,13 @@ function App() {
     <div className="row mt-3">
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        {user?.is_admin ? (<Route exact path="/interventions/:interventionId" element={<DashBoardViewDetails />}></Route>) : (<Route exact path="/not-authorized" element={<NotAuthorized/>}/>)}
-        {user?.is_admin ? (<Route exact path="/dashboard"element={<AdminDashboard user={user} />}/>) : (<Route exact path="/not-authorized" element={<NotAuthorized/>}/>)}
+        {user?.is_admin ? (<Route exact path="/interventions/:interventionId" element={<DashBoardViewDetails />}/>) : (<Route path="/*" element={<NotFound/>}/>)}
+        {user?.is_admin ? (<Route exact path="/dashboard"element={<AdminDashboard user={user} />}/>) : (<Route path="/*" element={<NotFound/>}/>)}
         <Route exact path="/login" element={<SignIn onLogin={setUser} />} />
         <Route exact path="/get-started" element={<SignUp onLogin={setUser} />} />
-        {user ? (<Route exact path="/user-landing" element={<UserLanding user={user}/>} />) : (<Route exact path="/not-authorized" element={<NotAuthorized/>}/>)}
-        {user?.is_admin ? (<Route exact path="/users" element={<UsersList user={user} />} />) : (<Route exact path="/not-authorized" element={<NotAuthorized/>}/>)}
-        {user ? (<Route exact path="/profile" element={<Profile user={user} />} />) : (<Route exact path="/not-authorized" element={<NotAuthorized/>}/>)}
+        {user ? (<Route exact path="/user-landing" element={<UserLanding user={user}/>} />) : (<Route path="/*" element={<NotFound/>}/>)}
+        {user?.is_admin ? (<Route exact path="/users" element={<UsersList user={user} />} />) : (<Route path="/*" element={<NotFound/>}/>)}
+        {user ? (<Route exact path="/profile" element={<Profile user={user} />} />) : (<Route path="/*" element={<NotFound/>}/>)}
         <Route path="/*" element={<NotFound/>}/>
       </Routes>
     </div>
