5e4630ee1d833187c68ee1c18df9b9bc5dc4d367
damaris <mdamarisnduku@gmail.com>
2022-10-30 22:16:53 +0300

    view datails styling

--- client/src/components/InterventionsViewDetails.js
+++ client/src/components/InterventionsViewDetails.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import AdminNavbar from "./AdminNavbar";
-import { DefaultPlayer as Video } from 'react-html5video';
+import PropTypes from "prop-types";
+// import { DefaultPlayer as Video } from 'react-html5video';
 import 'react-html5video/dist/styles.css';
 
-import a from "./videos/a.mp4"
-import e from "./images/e.jpg"
+// import a from "./videos/a.mp4"
+// import e from "./images/e.jpg"
 
 
-export default function InterventionsViewDetails({}){
+export default function InterventionsViewDetails(){
 
      const [status, setStatus] = useState("")
     // const [date, setDate] = useState("")
-    // const [title, setTitle] = useState("")
-    // const [video, setvideo] = useState("")
+    const [title, setTitle] = useState("")
+     const [video, setVideo] = useState("")
     const [image, setImage] = useState("")
     const [description, setDescription] = useState("")
     const [location, setLocation] = useState("")
@@ -30,7 +31,8 @@ export default function InterventionsViewDetails({}){
               console.log(interventions)
 
                setImage(interventions.image)
-            //    setVideo(interventions.video)
+              setVideo(interventions.video)
+              setTitle(interventions.title)
                setLocation(interventions.location)
                 setDescription(interventions.description)
                 setStatus(interventions.status)
@@ -49,20 +51,35 @@ export default function InterventionsViewDetails({}){
             <AdminNavbar/>
       <header style={{ display: "flex", marginTop: "150px", marginBottom: "10px"}}></header>
             <center>
-                <img src={image} style={{height: 500 + 'px', width: 600 + 'px',float:"right"}} />
-                { <Video autoPLay loop
+                <img src={image} style={{height: 350 + 'px', width: 450 + 'px',float:"right"}} />
+                <iframe
+      width="450"
+      height="350"
+      align="left"
+      src={`https://www.youtube.com/embed/${video}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+                {/* { <Video autoPLay loop
                 poster={e}
                 onCanPlayThrough={()=>{
 
                 }} style={{height: 500 + 'px', width: 600 + 'px',float:"left"}}>
                 <source src={a} type="video/webm"/>
-                </Video> }
+                </Video> } */}
+                 <h4 style={{color:"red"}}>{title}</h4>
                 <h4>{location}</h4>
-                <p>{description}</p>
+                <p style={{textAlign:"center"}}>{description}</p>
                 <h5>{status}</h5>
             </center> 
             </div> 
         </div>
     )
+    
 
 }
+InterventionsViewDetails.propTypes = {
+    video: PropTypes.string.isRequired
+  };
--- db/seeds.rb
+++ db/seeds.rb
@@ -49,7 +49,7 @@ puts "...seeding users 👨‍👩‍👧"
 puts '...seeding interventions 🚧'
 
      8.times do 
-        Intervention.create!(location: "Nairobi",
+        Intervention.create!(location: "Nairobi",title: "Poor Road",
             image: "https://images.unsplash.com/photo-1590719550920-7543bd5657a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
             video: "https://www.youtube.com/watch?v=GOISuk0pYIY", status: 
             "Under Investigation", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
@@ -59,7 +59,7 @@ puts '...seeding interventions 🚧'
 puts '...seeding redflags 🚩'
 
     8.times do 
-        Redflag.create!(location: "Nairobi",
+        Redflag.create!(location: "Nairobi",title: "Bribe",
             image: "https://images.unsplash.com/photo-1544449566-dee13645ade8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
             video: "https://www.youtube.com/watch?v=GOISuk0pYIY", status: 
             "Under Investigation", description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
